54549a9e01fde278c042578ee50e43233ba90078
valeria <hei.valeria.2@gmail.com>
2026-02-22 14:58:07 +0300

    feat: implement total votes count (Q1)

--- src/main/java/org/spring/DataRetriever.java
+++ src/main/java/org/spring/DataRetriever.java
@@ -1,4 +1,33 @@
 package org.spring;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataRetriever {
+
+    public long countAllVotes() {
+        long total = 0;
+
+        String sql = """
+        SELECT COUNT(id) AS total 
+        FROM vote
+    """;
+
+        DBConnection dbConnection = new DBConnection();
+        try (Connection conn = dbConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                total = rs.getLong("total");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erreur Q1 : " + e.getMessage());
+        }
+        return total;
+    }
 }
